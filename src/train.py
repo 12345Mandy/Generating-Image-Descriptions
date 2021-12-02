58b6d153f80e613df0d193e8018baab66f4b0af8
rrajesh1 <ria_rajesh@brown.edu>
2021-12-02 13:06:01 -0500

    pytorch for train

--- src/train.py
+++ src/train.py
@@ -1,25 +1,16 @@
 
+import numpy as np
+import pytorch as torch
 from numpy import array
 from pickle import load
-from tensorflow.python.keras.preprocessing.sequence import pad_sequences
-from tensorflow.python.keras.layers import *
-from tensorflow.python.keras.engine.input_layer import Input
-from tensorflow.python.keras.models import Model
-from keras_preprocessing.text import Tokenizer
-from tensorflow.python.keras.utils.np_utils import to_categorical
-from tensorflow.python.keras.utils.vis_utils import plot_model
-# from keras.preprocessing.text import Tokenizer
-# from keras.preprocessing.sequence import pad_sequences
-# from keras.utils import to_categorical
-# from keras.utils import plot_model
-# from keras.models import Model
-# from keras.layers import Input
-# from keras.layers import Dense
-# from keras.layers import LSTM
-# from keras.layers import Embedding
-# from keras.layers import Dropout
-# from keras.layers.merge import add
-# from keras.callbacks import ModelCheckpoint
+from torch.nn.utils.rnn import pad_sequence
+# from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+# from tensorflow.python.keras.layers import *
+# from tensorflow.python.keras.engine.input_layer import Input
+# from tensorflow.python.keras.models import Model
+# from keras_preprocessing.text import Tokenizer
+# from tensorflow.python.keras.utils.np_utils import to_categorical
+# from tensorflow.python.keras.utils.vis_utils import plot_model
  
 # load doc into memory
 def load_doc(filename):
@@ -82,12 +73,15 @@ def to_lines(descriptions):
 	return all_desc
  
 # fit a tokenizer given caption descriptions
-def create_tokenizer(descriptions):
+def create_tokenizer(descriptions): #this just needs to be manually changed to a dictionary
 	lines = to_lines(descriptions)
-	tokenizer = Tokenizer()
-	tokenizer.fit_on_texts(lines)
-	return tokenizer
- 
+	# tokenizer = Tokenizer()
+	# tokenizer.fit_on_texts(lines)
+	# return tokenizervocab = set(train_words) # collects all unique words in our dataset (vocab)
+	vocab = set(lines)
+	word2id = {w: i for i, w in enumerate(list(vocab))}
+	return word2id
+
 # calculate the length of the description with the most words
 def max_length(descriptions):
 	lines = to_lines(descriptions)
@@ -105,9 +99,10 @@ def create_sequences(tokenizer, max_length, desc_list, photo, vocab_size):
 			# split into input and output pair
 			in_seq, out_seq = seq[:i], seq[i]
 			# pad input sequence
-			in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+			in_seq = pad_sequence([in_seq], maxlen=max_length)[0]
 			# encode output sequence
-			out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+			#out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+			out_seq = np.eye(vocab_size, dtype='uint8')[out_seq][0]
 			# store
 			X1.append(photo)
 			X2.append(in_seq)
@@ -117,18 +112,18 @@ def create_sequences(tokenizer, max_length, desc_list, photo, vocab_size):
 # define the captioning model
 def define_model(vocab_size, max_length):
 	# feature extractor model
-	inputs1 = Input(shape=(4096,))
-	fe1 = Dropout(0.5)(inputs1)
-	fe2 = Dense(256, activation='relu')(fe1)
+	inputs1 = Input(shape=(4096,)) #dont know equivalent for this - come back
+	layer1 = torch.nn.dropout(0.5)(inputs1)
+	layer2 = torch.nn.linear(256, activation='relu')(layer1)
 	# sequence model
-	inputs2 = Input(shape=(max_length,))
-	se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
-	se2 = Dropout(0.5)(se1)
-	se3 = LSTM(256)(se2)
+	inputs2 = Input(shape=(max_length,)) #dont know equivalent for this - come back 
+	layer1_2 = torch.nn.Embedding(vocab_size, 256)(inputs2)
+	layer2_2 = torch.nn.dropout(0.5)(layer1_2)
+	se3 = torch.nn.LSTM(256)(layer2_2)
 	# decoder model
 	decoder1 = add([fe2, se3])
-	decoder2 = Dense(256, activation='relu')(decoder1)
-	outputs = Dense(vocab_size, activation='softmax')(decoder2)
+	decoder2 = torch.nn.linear(256, activation='relu')(decoder1)
+	outputs = torch.nn.linear(vocab_size, activation='softmax')(decoder2)
 	# tie it together [image, seq] [word]
 	model = Model(inputs=[inputs1, inputs2], outputs=outputs)
 	# compile model
